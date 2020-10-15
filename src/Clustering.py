7dda9104dde87f6f0907bb8fda837fafab356d10
Cheereus <fanwei1995@hotmail.com>
2020-10-15 18:18:33 +0800

    add spectral clustering

--- src/Clustering.py
+++ src/Clustering.py
@@ -1,35 +1,33 @@
-'''
-Description: 
-Author: CheeReus_11
-Date: 2020-08-10 07:33:24
-LastEditTime: 2020-08-10 16:38:45
-LastEditors: CheeReus_11
-'''
 import numpy as np
-from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.cluster import KMeans, AgglomerativeClustering, SpectralClustering
 from sklearn.neighbors import KNeighborsClassifier
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import matplotlib.pyplot as plt
+
 
 def k_means(X, k):
     k_m_model = KMeans(n_clusters=k, max_iter=300, n_init=40, init='k-means++', n_jobs=-1)
     k_m_model.fit(X)
     return k_m_model.labels_.tolist()
 
+
 def knn(X, y, k):
     knn_model = KNeighborsClassifier(n_neighbors=k)
     knn_model.fit(X, y)
     return knn_model
 
+
 def hca(X, k=None):
     hca_model = linkage(X, 'ward')
     return hca_model
+
 
 # dendogram for hca
 def hca_dendrogram(model):
     plt.figure(figsize=(50, 10))
     dendrogram(model, leaf_rotation=90., leaf_font_size=8)
     plt.show()
+
 
 # labels of hca
 def hca_labels(model, n_clusters):
---
+++ src/spectral.py
@@ -0,0 +1,37 @@
+from sklearn.cluster import SpectralClustering
+
+from ReadData import read_from_mat
+from DimensionReduction import t_SNE, get_pca
+from Utils import get_color, draw_scatter
+import numpy as np
+
+# read data
+X = read_from_mat('data/corr/A_islet.mat')['A']
+
+print(X.shape)
+
+# read labels
+labels = read_from_mat('data/corr/Labels_islet.mat')['Labels']
+labels = [i[0][0] for i in labels]
+
+# dimenison reduction
+# t-SNE
+dim_data = t_SNE(X, perp=5, with_normalize=True)
+
+# PCA
+# dim_data, ratio, result = get_pca(X, c=2, with_normalize=True)
+# print(ratio)
+
+# get two coordinates
+x = [i[0] for i in dim_data]
+y = [i[1] for i in dim_data]
+#
+labels_predict = SpectralClustering(n_clusters=7, affinity='nearest_neighbors').fit_predict(X)
+print(labels_predict)
+
+# get color list based on labels
+default_colors = ['c', 'b', 'g', 'r', 'm', 'y', 'k']
+colors = get_color(labels_predict)
+
+# plot
+draw_scatter(x, y, labels_predict, colors)
