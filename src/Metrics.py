7b7f19d3d1821eb0c7ae2c01cfbef4e7bee412d5
Cheereus <fanwei1995@hotmail.com>
2020-10-16 11:51:27 +0800

    reformat files

--- src/Metrics.py
+++ src/Metrics.py
@@ -1,12 +1,5 @@
-'''
-Description: 
-Author: CheeReus_11
-Date: 2020-08-10 16:38:03
-LastEditTime: 2020-08-10 16:49:56
-LastEditors: CheeReus_11
-'''
-
 from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score, f1_score
+
 
 def accuracy(predict_labels, true_labels):
     if len(predict_labels) != len(true_labels):
@@ -19,11 +12,14 @@ def accuracy(predict_labels, true_labels):
             correct += 1
     return correct / label_length
 
+
 def ARI(true_labels, predict_labels):
     return adjusted_rand_score(true_labels, predict_labels)
 
+
 def NMI(true_labels, predict_labels):
     return normalized_mutual_info_score(true_labels, predict_labels)
+
 
 def F1(true_labels, predict_labels):
     return f1_score(true_labels, predict_labels, average='weighted')
