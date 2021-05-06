adc3e7d3f8e0e6e7c17faf10304d5c353ad8ea96
Cheereus <fanwei1995@hotmail.com>
2021-05-06 17:07:44 +0800

    updates

--- src/read_temp.py
+++ src/read_temp.py
@@ -2,16 +2,18 @@ import joblib
 import numpy as np
 from ReadData import read_from_csv, read_from_txt
 
-# data = read_from_csv('data/GSE86469.csv')
-data = read_from_txt('data/GSE57872_GBM_data_matrix.txt')
-# print(data)
-labels = data[0][:430]
-labels = [i.split('_')[0] for i in labels]
-print(labels)
+data = read_from_txt('data/GSE60361.txt')
 
-data = [i[1:431] for i in data[1:]]
-data = np.array(data)
-print(data)
-print(data.shape)
-joblib.dump(data.T.astype(np.float), 'datasets/GSE57872.pkl')
-joblib.dump(labels, 'datasets/GSE57872_labels.pkl')
+labels = data[1][3:]
+# labels = ['d0' for _ in range(data.shape[0]-1)]
+# print(len(labels))
+# #
+# data = data[1:]
+data_list = []
+for i in range(10, data.shape[0]):
+    data_list.append(data[i][2:])
+
+data_list = np.array(data_list)
+print(data_list.shape)
+joblib.dump(data_list.astype(np.float32).T, 'datasets/GSE60361.pkl')
+joblib.dump(labels, 'datasets/GSE60361_labels.pkl')
